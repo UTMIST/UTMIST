272bbf4b7298e10f7e83516a9412717024357012
Ambrose Ling <lingambrose@gmail.com>
2025-05-19 19:49:57 -0400

    Update Navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -3,6 +3,7 @@ import "../styles/gradients.css";
 import Image from "next/image";
 import Link from "next/link"; // Import Link from next/link
 import logo from "../assets/logos/utmist-logo-small.svg";
+import lightMode from "../assets/icons/light-mode.svg";
 
 export default function Navbar() {
   return (
@@ -18,6 +19,7 @@ export default function Navbar() {
         <li><Link href="/#about-us" className="nav-item">About Us</Link></li>
         <li><Link href="/projects" className="nav-item">Projects</Link></li>
         <li><Link href="/blog" className="nav-item">Blog</Link></li>
+        <li><Link href="/sponsors" className="nav-item">Sponsors</Link></li>
         <li><Link href="/events" className="nav-item">Events</Link></li>
         <li><Link href="/careers" className="nav-item">Careers</Link></li>
       </ul>
@@ -26,7 +28,9 @@ export default function Navbar() {
         <button className="nav-button">
           Log In
         </button>
+      <Image src={lightMode} alt="UTMIST Logo" className="hover:brightness-75 transition" width={32} height={32} />
       </div>
+
     </nav>
   );
 }
