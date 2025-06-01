4bb73d8d8e56794f1939bafc372580105aaaf732
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2025-06-01 13:41:15 -0400

    fix image formatting

--- client/src/assets/photos/speakers/index.ts
+++ client/src/assets/photos/speakers/index.ts
@@ -4,7 +4,6 @@ import Tyler from "@/assets/photos/speakers/Tyler.png";
 import Marcelo from "@/assets/photos/speakers/Marcelo.png";
 import Mustafa from "@/assets/photos/speakers/Mustafa.png";
 import Priyanka from "@/assets/photos/speakers/Priyanka.png";
-// casing error is fine if it builds on ci (linux problem)
-import Ryan from "@/assets/photos/speakers/Ryan.JPG";
+import Ryan from "@/assets/photos/speakers/Ryan.jpg";
 
 export { Amrit, Devang, Tyler, Marcelo, Mustafa, Priyanka, Ryan };
