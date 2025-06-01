baddd91a868309520c6887af46594fd0f3f9b465
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2025-06-01 13:37:46 -0400

    fix image import

--- client/src/assets/photos/speakers/index.ts
+++ client/src/assets/photos/speakers/index.ts
@@ -4,6 +4,7 @@ import Tyler from "@/assets/photos/speakers/Tyler.png";
 import Marcelo from "@/assets/photos/speakers/Marcelo.png";
 import Mustafa from "@/assets/photos/speakers/Mustafa.png";
 import Priyanka from "@/assets/photos/speakers/Priyanka.png";
-import Ryan from "@/assets/photos/speakers/Ryan.jpg";
+// casing error is fine if it builds on ci (linux problem)
+import Ryan from "@/assets/photos/speakers/Ryan.JPG";
 
 export { Amrit, Devang, Tyler, Marcelo, Mustafa, Priyanka, Ryan };
