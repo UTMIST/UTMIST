5c3f2110173795962b45dfd1256746d328c774d4
William Lam <williamntlam@gmail.com>
2025-06-01 16:13:45 -0400

    feat: implemented an apply here button that will redirect to the apply page along with a disclaimer of due date for applications [skip ci]

--- client/src/app/startups/page.tsx
+++ client/src/app/startups/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
@@ -16,13 +17,25 @@ const StartupsPage = () => {
           </h1>
           <p className="text-lg m-0 leading-snug text-center">
             <span className="block ">
-              Help build AI startups with Canada’s largest student-led
+              Help build AI startups with Canada's largest student-led
             </span>
             <span className="block ">
               organization for Artificial Intelligence and Machine
             </span>
             <span className="block ">Learning</span>
           </p>
+          <div>
+            <Link
+              href="/apply"
+              className="bg-gradient-to-r from-indigo-400 to-blue-800 text-white px-12 py-1 rounded-full text-lg font-medium shadow mx-auto"
+            >
+              Apply
+            </Link>
+            <p className="text-xs text-gray-500 text-center mt-2">
+              <span className="block">Applications close</span>
+              <span className="block">Sept 31st</span>
+            </p>
+          </div>
         </div>
       </section>
       <section>{/* Startups Information */}</section>
