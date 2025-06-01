ee8c4cdadc74fb8821c2a7c61fc7db4cedcd3a03
William Lam <williamntlam@gmail.com>
2025-06-01 16:00:37 -0400

    feat: implemented title and description of startup page in the hero section

--- client/src/app/startups/page.tsx
+++ client/src/app/startups/page.tsx
@@ -1,9 +1,41 @@
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
 const StartupsPage = () => {
-    return (
-        <main>
-            <h1>Startups</h1>
-        </main>
-    )
-}
+  return (
+    <main>
+      <section>
+        {/* Hero Component */}
+        <div className="flex flex-col gap-4 text-center">
+          <h1 className={`${roboto.className} font-bold text-4xl`}>
+            Innovation @ UTMIST
+          </h1>
+          <p className="text-lg m-0 leading-snug text-center">
+            <span className="block ">
+              Help build AI startups with Canada’s largest student-led
+            </span>
+            <span className="block ">
+              organization for Artificial Intelligence and Machine
+            </span>
+            <span className="block ">Learning</span>
+          </p>
+        </div>
+      </section>
+      <section>{/* Startups Information */}</section>
+      <div>
+        <div>
+          <section>{/* Investors slider with article cards */}</section>
+        </div>
+        <div>
+          <section>{/* Guess Speakers slider with article cards */}</section>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default StartupsPage;
