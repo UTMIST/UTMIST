460da860e538bdbeb36ecfb22f8d9f33fb0d53e3
Yanzhen Chen <117863711+YheChen@users.noreply.github.com>
2025-05-31 21:55:01 -0400

    Converted several images to WEBP format for better loading (#33)
    * changed several images/company logos from png to
    webp format to improve loading speed, also changed
    imports to match the new file types
    
    * edited the import that I missed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,8 @@
 import HeroCard from "@/components/cards/hero-card";
 import Image from "next/image";
-import utmistLogo from "@/assets/logos/utmist-logo-large.svg"; 
-import execPhoto from "@/assets/photos/eigenai-exec-photo.png";
-import copresPhoto from "@/assets/photos/eigenai-copres-speech.png";
+import utmistLogo from "@/assets/logos/utmist-logo-large.svg";
+import execPhoto from "@/assets/photos/eigenai-exec-photo.webp";
+import copresPhoto from "@/assets/photos/eigenai-copres-speech.webp";
 import Sponsors from "@/components/sponsors";
 import Statistics from "@/components/stats";
 import Events from "@/components/events";
@@ -11,18 +11,25 @@ import FAQ from "@/components/faq";
 import Link from "next/link"; // Import Link from next/link
 
 export default function Home() {
-    return (
-      <main>
-        {/*Hero Section */}
-        <div className="cta-container">
+  return (
+    <main>
+      {/*Hero Section */}
+      <div className="cta-container">
         <div className="cta-content">
           <h2 className="cta-title">Clear The MIST</h2>
           <p className="cta-subtitle">
             University of Toronto Machine Intelligence Student Team
           </p>
           <div className="cta-buttons">
-            <Link href="/careers" className="primary-button" >Join Us</Link>
-            <Link href="mailto:utorontomist@gmail.com" className="secondary-button">Contact Us</Link>
+            <Link href="/careers" className="primary-button">
+              Join Us
+            </Link>
+            <Link
+              href="mailto:utorontomist@gmail.com"
+              className="secondary-button"
+            >
+              Contact Us
+            </Link>
           </div>
         </div>
         <div className="cta-logo">
@@ -34,9 +41,7 @@ export default function Home() {
             priority
           />
         </div>
-        <div className="cta-big-title">
-          UTMIST
-        </div>
+        <div className="cta-big-title">UTMIST</div>
       </div>
 
       {/*Sponsors Section */}
@@ -44,41 +49,43 @@ export default function Home() {
 
       {/*Statistics Section */}
       <div className="flex flex-col gap-8 justify-center items-center my-16">
-      <p className="text-4xl font-bold text-center max-w-3xl text-black">
-        UTMIST is Canada&apos;s largest student-lead organization for Artificial Intelligence and Machine Learning
-      </p>
-      <Statistics/>
+        <p className="text-4xl font-bold text-center max-w-3xl text-black">
+          UTMIST is Canada&apos;s largest student-lead organization for
+          Artificial Intelligence and Machine Learning
+        </p>
+        <Statistics />
       </div>
 
       {/*Who We Are / Mission Section */}
       <section id="about-us">
-      <div  className="hero-container">
-        <HeroCard 
-        image={execPhoto}
-        title="Who We Are"
-        description="At UTMIST, we produce in-house academic content, spearhead design teams to collaborate on machine learning and infrastructure projects, and host annual milestone events, empowering our members to become the next generation of engineers, researchers, and leaders in AI/ML."
-      />
-      <div className= "hero-line" />
-      <HeroCard 
-        image={copresPhoto}
-        title="Our Mission"
-        description="Our mission is to empower students of all skill levels and experiences to foster their passion and accelerate their career in Artificial Intelligence and Machine Learning. Through workshops, conference, hackthons and many other initiatives, we hope to provide the best oppurtunities to UofT students in this field."
-      />
-    </div> 
+        <div className="hero-container">
+          <HeroCard
+            image={execPhoto}
+            title="Who We Are"
+            description="At UTMIST, we produce in-house academic content, spearhead design teams to collaborate on machine learning and infrastructure projects, and host annual milestone events, empowering our members to become the next generation of engineers, researchers, and leaders in AI/ML."
+          />
+          <div className="hero-line" />
+          <HeroCard
+            image={copresPhoto}
+            title="Our Mission"
+            description="Our mission is to empower students of all skill levels and experiences to foster their passion and accelerate their career in Artificial Intelligence and Machine Learning. Through workshops, conference, hackthons and many other initiatives, we hope to provide the best oppurtunities to UofT students in this field."
+          />
+        </div>
       </section>
 
-    {/*Events Section */}
-    <Events />
+      {/*Events Section */}
+      <Events />
 
-    <div className="flex flex-col gap-8 justify-center items-center my-16">
-    <p className="text-4xl font-bold text-center max-w-3xl text-black">
-    We bring together students, experts, and innovators to explore AI from every angle</p>
+      <div className="flex flex-col gap-8 justify-center items-center my-16">
+        <p className="text-4xl font-bold text-center max-w-3xl text-black">
+          We bring together students, experts, and innovators to explore AI from
+          every angle
+        </p>
       </div>
 
-      <ValueProps/>
-      
-      <FAQ/>
-    </main>
+      <ValueProps />
 
+      <FAQ />
+    </main>
   );
 }
