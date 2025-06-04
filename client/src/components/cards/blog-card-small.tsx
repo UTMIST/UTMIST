cc209339c1eae7721bde214b73c29677357ede20
Ethan Qiu <ethanqiu@gmail.com>
2025-06-04 15:09:20 -0400

    Fix spacing on cards

--- client/src/components/cards/blog-card-small.tsx
+++ client/src/components/cards/blog-card-small.tsx
@@ -5,8 +5,8 @@ import dummy from "@/assets/photos/fibseq.webp";
 export default function BlogCardSmall({ title, date, author, image, url }: EventCardProps) {
   return (
     <a href={url} target="_blank" rel="noopener noreferrer">
-    <div className="w-full aspect-[4/3] rounded-lg overflow-hidden border border-gray-200 bg-white shadow-sm">
-      <div className="relative w-full h-[60%]">
+    <div className="w-full aspect-[4/3] min-h-[250px] rounded-lg overflow-hidden border border-gray-200 bg-white shadow-sm">
+      <div className="relative w-full h-[70%] md:h-[55%]">
         <Image
           src={image || dummy}
           alt={title}
@@ -14,10 +14,12 @@ export default function BlogCardSmall({ title, date, author, image, url }: Event
           className="object-cover object-center"
         />
       </div>
-      <div className="p-3 h-[40%]">
+      <div className="p-2 md:p-3 h-[30%] md:h-[45%] flex flex-col justify-between">
         <h3 className="text-black font-semibold leading-tight line-clamp-2 text-base">{title}</h3>
-        <p className="text-gray-500 text-xs mt-1">{date}</p>
-        <p className="text-gray-500 text-xs">{author}</p>
+        <div>
+          <p className="text-gray-500 text-xs">{date}</p>
+          <p className="text-gray-500 text-xs">{author}</p>
+        </div>
       </div>
     </div>
     </a>
