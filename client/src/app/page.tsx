2d9ea8b4ba86f31df648e2ed866b1f6c6ff187ab
YheChen <yanzhenchen123@gmail.com>
2025-06-05 14:37:40 -0400

    everything on home page above events section is now styled well for mobile viewing

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
 
       {/*Statistics Section */}
       <div className="flex flex-col gap-8 justify-center items-center my-16">
-        <p className="text-4xl font-bold text-center max-w-3xl text-black">
+        <p className="text-4xl font-bold text-center max-w-3xl text-black px-8 md:px-0">
           UTMIST is Canada&apos;s largest student-lead organization for
           Artificial Intelligence and Machine Learning
         </p>
