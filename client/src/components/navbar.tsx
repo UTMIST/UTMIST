6580a5c05b8d903496891de2aace0585fc6b8d67
YheChen <yanzhenchen123@gmail.com>
2025-06-05 13:04:38 -0400

    footer looks better on mobile now, still dealing with the navbar, styling needs lots of work

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,40 +1,127 @@
+"use client";
 import "../styles/navbar.css";
 import "../styles/gradients.css";
 import Image from "next/image";
-import Link from "next/link"; // Import Link from next/link
+import Link from "next/link";
 import logo from "../assets/logos/utmist-logo-small.svg";
-// import lightMode from "../assets/icons/light-mode.svg"; // Uncomment if you have a light mode icon
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="navbar-container">
-      <Link href="/" passHref>
-        <div className="flex items-center space-x-2 cursor-pointer">
-          <Image src={logo} alt="UTMIST Logo" width={32} height={32} />
-          <span className="navbar-logo-text">UTMIST</span>
-        </div>
-      </Link>
+    <>
+      <nav className="navbar-container">
+        <Link href="/" passHref>
+          <div className="flex items-center space-x-2 cursor-pointer">
+            <Image src={logo} alt="UTMIST Logo" width={32} height={32} />
+            <span className="navbar-logo-text">UTMIST</span>
+          </div>
+        </Link>
 
-      <ul className="flex space-x-10">
-        <li><Link href="/#about-us" className="nav-item">About Us</Link></li>
-        <li><Link href="/projects" className="nav-item">Projects</Link></li>
-        <li><Link href="/blog" className="nav-item">Blog</Link></li>
-        <li><Link href="/sponsors" className="nav-item">Sponsors</Link></li>
-        <li><Link href="/events" className="nav-item">Events</Link></li>
-        <li><Link href="/careers" className="nav-item">Careers</Link></li>
-      </ul>
+        {/* Full Nav (Desktop only) */}
+        <ul className="nav-links">
+          <li>
+            <Link href="/#about-us" className="nav-item">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link href="/projects" className="nav-item">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog" className="nav-item">
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link href="/sponsors" className="nav-item">
+              Sponsors
+            </Link>
+          </li>
+          <li>
+            <Link href="/events" className="nav-item">
+              Events
+            </Link>
+          </li>
+          <li>
+            <Link href="/careers" className="nav-item">
+              Careers
+            </Link>
+          </li>
+        </ul>
 
-      <div className="flex items-center space-x-4">
-        {/* Disable Log In Button Until Applicant Portal Completion */}
-        {/* <button className="nav-button">
-          Log In
-        </button> */}
+        {/* Hamburger (Mobile only) */}
+        <button
+          className="hamburger"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle menu"
+        >
+          ☰
+        </button>
+      </nav>
 
-        {/* Disable Color Mode Toggle until Completion */}
-        {/* <Image src={lightMode} alt="Color Mode Logo" className="hover:brightness-75 transition" width={32} height={32} /> */}
-      </div>
-
-    </nav>
+      {/* Mobile Menu Dropdown */}
+      {isOpen && (
+        <ul className="mobile-dropdown">
+          <li>
+            <Link
+              href="/#about-us"
+              onClick={() => setIsOpen(false)}
+              className="nav-item"
+            >
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/projects"
+              onClick={() => setIsOpen(false)}
+              className="nav-item"
+            >
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/blog"
+              onClick={() => setIsOpen(false)}
+              className="nav-item"
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/sponsors"
+              onClick={() => setIsOpen(false)}
+              className="nav-item"
+            >
+              Sponsors
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/events"
+              onClick={() => setIsOpen(false)}
+              className="nav-item"
+            >
+              Events
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/careers"
+              onClick={() => setIsOpen(false)}
+              className="nav-item"
+            >
+              Careers
+            </Link>
+          </li>
+        </ul>
+      )}
+    </>
   );
 }
