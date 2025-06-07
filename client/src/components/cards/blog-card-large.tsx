85900296180762f97af0716ed7f4abe026d51e61
YheChen <yanzhenchen123@gmail.com>
2025-06-07 11:39:51 -0400

    blog has mobile interface done, only career is left

--- client/src/components/cards/blog-card-large.tsx
+++ client/src/components/cards/blog-card-large.tsx
@@ -1,24 +1,23 @@
 import Image from "next/image";
 import { EventCardProps } from "@/types/Blog";
 
-export default function BlogCardLarge({ title, date, image, url }: EventCardProps) {
+export default function BlogCardLarge({
+  title,
+  date,
+  image,
+  url,
+}: EventCardProps) {
   return (
     <a href={url} target="_blank" rel="noopener noreferrer">
-    <div className="relative w-[300px] h-[200px] min-h-[340px] min-w-[400px] rounded-lg overflow-hidden shadow-md">
-      <Image
-        src={image}
-        alt={title}
-        fill
-        className="object-cover"
-        priority
-      />
-      <div className="absolute bottom-0 left-0 w-full p-4">
-        <h2 className="text-black font-semibold text-4xl leading-snug">
-          {title}
-        </h2>
-        <p className="text-gray-700 text-xs mt-1">{date}</p>
+      <div className="relative w-[240px] h-[160px] sm:w-[300px] sm:h-[200px] sm:min-h-[340px] sm:min-w-[400px] rounded-lg overflow-hidden shadow-md">
+        <Image src={image} alt={title} fill className="object-cover" priority />
+        <div className="absolute bottom-0 left-0 w-full p-4">
+          <h2 className="text-black font-semibold text-sm sm:text-4xl leading-snug">
+            {title}
+          </h2>
+          <p className="text-gray-700 text-xs mt-1">{date}</p>
+        </div>
       </div>
-    </div>
-  </a>
+    </a>
   );
 }
