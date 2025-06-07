68a1fc01674bd7bc31506219b85bb92ec6e09477
YheChen <yanzhenchen123@gmail.com>
2025-06-07 11:39:51 -0400

    blog has mobile interface done, only career is left

--- client/src/components/cards/blog-card-large.tsx
+++ client/src/components/cards/blog-card-large.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { EventCardProps } from "@/types/Blog";
 import dummy from "@/assets/photos/fibseq.webp";
 
+<<<<<<< HEAD
 export default function BlogCardLarge({ title, date, author, image, url }: EventCardProps) {
   return (
     <a href={url} target="_blank" rel="noopener noreferrer">
@@ -37,6 +38,23 @@ export default function BlogCardLarge({ title, date, author, image, url }: Event
               {author}
             </p>
           </div>
+=======
+export default function BlogCardLarge({
+  title,
+  date,
+  image,
+  url,
+}: EventCardProps) {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <div className="relative w-[240px] h-[160px] sm:w-[300px] sm:h-[200px] sm:min-h-[340px] sm:min-w-[400px] rounded-lg overflow-hidden shadow-md">
+        <Image src={image} alt={title} fill className="object-cover" priority />
+        <div className="absolute bottom-0 left-0 w-full p-4">
+          <h2 className="text-black font-semibold text-sm sm:text-4xl leading-snug">
+            {title}
+          </h2>
+          <p className="text-gray-700 text-xs mt-1">{date}</p>
+>>>>>>> 8590029 (blog has mobile interface done, only career is)
         </div>
       </div>
     </a>
--- client/src/components/cards/blog-card-small.tsx
+++ client/src/components/cards/blog-card-small.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { EventCardProps } from "@/types/Blog";
 import dummy from "@/assets/photos/fibseq.webp";
 
+<<<<<<< HEAD
 export default function BlogCardSmall({ title, date, author, image, url }: EventCardProps) {
   return (
     <a href={url} target="_blank" rel="noopener noreferrer">
@@ -22,6 +23,32 @@ export default function BlogCardSmall({ title, date, author, image, url }: Event
         </div>
       </div>
     </div>
+=======
+export default function BlogCardSmall({
+  title,
+  date,
+  image,
+  url,
+}: EventCardProps) {
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <div className="w-[240px]">
+        <div className="relative w-full h-[160px] rounded-xl overflow-hidden border border-gray-200">
+          <Image
+            src={image}
+            alt={title}
+            fill
+            className="object-cover object-center"
+          />
+        </div>
+        <div className="mt-2">
+          <h3 className="text-black font-semibold leading-snug line-clamp-2">
+            {title}
+          </h3>
+          <p className="text-xs text-gray-500">{date}</p>
+        </div>
+      </div>
+>>>>>>> 8590029 (blog has mobile interface done, only career is)
     </a>
   );
 }
