f570372ac7616a084da263aadf3b6ddad9ead308
Ethan Qiu <ethanqiu@gmail.com>
2025-06-08 19:42:31 -0400

    Add events

--- server/server/urls.py
+++ server/server/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/blog/', include('blog.urls')),
+    path('api/events/', include('events.urls')),  # Events URLs
 ]
