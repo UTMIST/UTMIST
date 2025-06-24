2d5821240e4b2d834cb3f100bf3c80dc7405a954
William Nhut Lam <163342709+williamntlam@users.noreply.github.com>
2025-06-23 23:03:05 -0400

    [APPLICANT-PORTAL]: Create an Application Form (#56)
    * feat: created interfaces for application form information
    
    * feat: created structure of the apply page
    
    * feat: added area code and hyphens for phone number input [skip ci]
    
    * feat: added validation checking for email input [skip ci]
    
    * fix: invalid email error does not show for the empty string [skip ci]
    
    * feat: made date selection a drop down [skip ci]
    
    * feat: added labels and updated work experience module to add a 'currently work here' checkbox
    
    * feat: made the degree select a drop down [skip ci]
    
    * feat: made field of study input a drop down [skip ci]
    
    * feat: made school input a dropdown [skip ci]
    
    * feat: added more details for the area code by adding what country has the specified area code in parentheses [skip ci]
    
    * feat: select country and then all provinces / states are shown for the specified country
    
    * feat: postal code automatically formatted to match that of the specified country
    
    * feat: made mobile responsiveness better for the start / end date selection for the work experience section [skip ci]
    
    * style: added icons and text for the button that adds more work experience and for the resume upload
    
    * chore: add back navbar and footer
    
    * refactor: broke down code into components and updated POST fetch request logic.
    
    * feat: added resume pdf file validation and size limit as well as a preview underneath
    
    * style: made file size error text bolded
    
    * chore: fixed error parameter not being used when POST fetch request fails
    
    * refactor: refactored code for scalability etc by breaking down the code into components
    
    * style: reversed order of year selection so that must recent years come first
    
    * style: fixed start and end date input misalignments
    
    * style: added more future years in graduation date selection
    
    * style: fixed layout of start and end date selection
    
    * feat: added phone number validation for selected country
    
    * fix: removed area code check
    
    * refactor: changed degree dropdown to education level with options 'diploma', 'undergraduate', 'graduate', and 'other'
    
    * delete: removed work experience section
    
    * refactor: moved types and interfaces for the apply page into a separate file in the /src/types directory [skip ci]
    
    * delete: removed redundant types file for the apply page
    
    * feat: added a 'why join utmist' text field [skip ci]
    
    * style: fixed arrows being too close to the right side [skip ci]
    
    * feat: added the 'whyJoin' statement to the ApplicationForm interface [skip ci]
    
    * refactor: resume icon is imported now [skip ci]
    
    * style: made styling for the inputs more consistent [skip ci]
    
    * feat: used university search API for university input under education section
    
    * feat: updated majors list
    
    * feat: text box appears to specify 'other' option for country
    
    * refactor: import resume icon image instead of using svg code
    
    * refactor: import resume icon image instead of using svg code
    
    * refactor: used a single styled 'select' component for cross-browser compatibility
    
    * refactor: removed work experience code
    
    * fix: specified type for university api
    
    * feat: added 'other' text field for education and field of study
    
    * delete: removed redundant state variables for degree and major information
    
    * fix: used 'string' instead of 'String' for type
    
    * style: changed foreground to #0000000
    
    * Remove node_modules from tracking and add to .gitignore
    
    * chore: remove node_modules

---
+++ client/src/types/apply.ts
@@ -0,0 +1,40 @@
+export interface PersonalInformation {
+    firstName: string;
+    lastName: string;
+    email: string;
+    areaCode: string;
+    phoneNumber: string;
+}
+
+export interface ContactInformation {
+    country: string;
+    address: string;
+    city: string;
+    postalCode: string;
+    provinceOrState: string;
+}
+
+export interface EducationInformation {
+    school: string;
+    educationLevel: string;
+    fieldOfStudy: string;
+    graduationMonth: string;
+    graduationYear: string;
+    otherFieldOfStudy?: string;
+    otherEducationLevel?: string;
+}
+
+export interface ApplicationFormData {
+    personalInfo: PersonalInformation;
+    locationInfo: ContactInformation;
+    educationInfo: EducationInformation;
+    whyJoin: string;
+} 
+
+// Type for the response from the universities API
+export type UniversityAPIResponse = {
+    web_page: string;
+    country: string;
+    domain: string;
+    name: string;
+};
