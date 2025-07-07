365997601c04b3eee35e0678220a1ea7d5fc3eab
Ethan Qiu <ethanqiu@gmail.com>
2025-07-07 19:23:36 -0400

    Fix layout issue

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,15 +1,29 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import { Roboto, Space_Grotesk } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+// Importing fonts from Google Fonts
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"], // Added specific weights
+  variable: "--font-roboto",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-space-grotesk",
+  subsets: ["latin"],
+});
 
 //Metadata for the page
 export const metadata: Metadata = {
   title: "UTMIST",
-  description: "University of Toronto Machine Intelligence Student Team",
+  description: "UTMIST - Largest AI and ML student org in Canada",
+  icons: {
+    icon: "/UTMIST.ico",
+  },
 };
 
 // Root layout for the application
@@ -21,10 +35,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar/>
+      <body
+        className={`${roboto.variable} ${spaceGrotesk.variable} antialiased`}
+      >
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
