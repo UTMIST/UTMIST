d1f333af754e804311c3b481cee110d3ba569d78
Ethan Qiu <ethanqiu@gmail.com>
2025-07-08 23:03:58 -0400

    Remove django server from branch

--- server/accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile
-
-# Register UserProfile model with the admin interface
-admin.site.register(UserProfile)
--- server/blog/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import BlogPost
-
-class BlogPostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BlogPost
-        fields = ['id', 'title', 'author', 'content', 'url', 'image', 'date', 'updated', 'super_featured', 'featured']
-        read_only_fields = ['updated'] 
