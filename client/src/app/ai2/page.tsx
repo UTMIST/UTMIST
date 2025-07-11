52884b6a5a1011efc992f3501c8150b9a90827cb
Vivian Song <viviann.song006@gmail.com>
2025-07-11 00:28:34 -0400

    fixed special thanks section and added headshots

--- client/src/app/ai2/page.tsx
+++ client/src/app/ai2/page.tsx
@@ -13,12 +13,51 @@ import img2 from "@/assets/photos/slider/img2.png";
 import img3 from "@/assets/photos/slider/img3.png";
 import img4 from "@/assets/photos/slider/img4.png";
 
+// import headshots from "@/assets/photos/headshots";
+import andrew from "@/assets/photos/headshots/andrew_headshot.png";
+import doga from "@/assets/photos/headshots/Doga_Baskan.jpg";
+import kaden from "@/assets/photos/headshots/kaden-seto-headshot.jpg";
+import martin from "@/assets/photos/headshots/Martin_Tin.png";
+import matthew from "@/assets/photos/headshots/matthew_tamura_headshot.png";
+
 
 const slides = [
   img1.src,
   img2.src,
   img3.src,
   img4.src,
+];
+
+const specialThanks = [
+  {
+    name: "Andrew",
+    role: "Engineer @ UofT",
+    photo: andrew.src
+  },
+
+  {
+    name: "Doge Baskan",
+    role: "Organizer",
+    photo: doga.src
+  },
+  
+  {
+    name: "Kaden Seto",
+    role: "Organizer",
+    photo: kaden.src
+  },
+
+  {
+    name: "Martin Tin",
+    role: "Organizer",
+    photo: martin.src
+  },
+
+  {
+    name: "Matthew Tamura",
+    role: "Organizer",
+    photo: matthew.src
+  },
 ];
 
 export default function AI2Page() {
@@ -132,6 +171,22 @@ export default function AI2Page() {
         </div>
     </section>
     <section>
+    <h2 className="special-section-title">Special Thanks</h2>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-12 max-w-5xl mx-auto mb-40">
+      {specialThanks.map((person, i) => (
+        <div key={i} className="flex flex-col items-center">
+          <img
+            src={person.photo}
+            alt={person.name}
+            className="w-32 h-32 object-cover rounded-full mb-4 border-4 border-white shadow"
+          />
+          <div className="special-section-header">{person.name}</div>
+          <div className="text-gray-600 text-center">{person.role}</div>
+        </div>
+      ))}
+    </div>
+  </section>
+    {/* <section>
       <h2 className="special-section-title">Special Thanks</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-12 max-w-5xl mx-auto mb-40">
         {[...Array(6)].map((_, i) => (
@@ -142,7 +197,7 @@ export default function AI2Page() {
           </div>
         ))}
       </div>
-    </section>
+    </section> */}
     </div>
     </main>
   );
