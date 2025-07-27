625e0d59fe5fe1c14159197f73d749426449673c
KCui0327 <kenny.cui@mail.utoronto.ca>
2025-07-27 11:08:55 -0700

    Comment out unused imports for careers

--- client/src/app/careers/page.tsx
+++ client/src/app/careers/page.tsx
@@ -1,10 +1,10 @@
 import "@/styles/careers.css"  
-import positionsData from "@/assets/careers.json"
+// import positionsData from "@/assets/careers.json"
 import Image from "next/image"
 import blueTick from "@/assets/icons/blue-tick-icon.svg";
 import darkBlueTick from "@/assets/icons/dark-blue-tick-icon.svg";
 import HeroSection from "@/components/heroSection";
-import {Positions} from "@/types/careers";
+// import {Positions} from "@/types/careers";
 
 export default function CareersPage() {
 
