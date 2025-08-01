60fd0c0348b16afc9284bf0f38d24dbc765394af
e-ndorfin <zach.tang@mail.utoronto.ca>
2025-08-02 03:51:05 +0800

    dir fix in tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}", // include all page files
-    "./components/**/*.{js,ts,jsx,tsx}", // include all components
-    "./app/**/*.{js,ts,jsx,tsx}", // if you're using the app directory
+    "./src/pages/**/*.{js,ts,jsx,tsx}", // include all page files
+    "./src/components/**/*.{js,ts,jsx,tsx}", // include all components
+    "./src/app/**/*.{js,ts,jsx,tsx}", // if you're using the app directory
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       // Your custom colors, spacing, fonts, etc.
