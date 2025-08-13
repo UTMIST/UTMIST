aa1470a6456df25efd08e5ccbe29a641cb089615
Kenny Cui <kenny.cui@mail.utoronto.ca>
2025-08-12 19:32:52 -0700

    AI Squared Webpage (#111)
    * Created AI Squared webpage
    
    * AI Squared Webpage Refactor
    
    * Refactor headshots
    
    * Address CI errors
    
    * Remove "'"
    
    * Made styling changes
    
    * Added new assets
    
    * timeline and style changes
    
    * style changes
    
    * Added new Timeline and fix card issues for features
    
    * ai2 page styling and new sections
    
    * remove dark mode
    
    * declaration file
    
    * workaround on eslint on type check
    
    * make title optional
    
    ---------
    
    Co-authored-by: Vivian Song <viviann.song006@gmail.com>
    Co-authored-by: Ambrose Ling <lingambrose@gmail.com>
    Co-authored-by: unknown <j.chen1904@gmail.com>

---
+++ client/src/assets/photos/ai2/index.ts
@@ -0,0 +1,10 @@
+
+import cube from "@/assets/photos/ai2/cube.gif";
+import new_weapons from "@/assets/photos/ai2/new_weapons.png";
+import interactive_environment from "@/assets/photos/ai2/interactive_environment.png";
+import better_customization from "@/assets/photos/ai2/more_customization.png";
+import cube_1 from "@/assets/photos/ai2/ai2cube-1.png";
+import cube_2 from "@/assets/photos/ai2/ai2cube-2.png";
+import cube_3 from "@/assets/photos/ai2/ai2cube-3.png";
+import logo_sketch from "@/assets/photos/ai2/ai2_logo_sketch.png"
+export { cube, new_weapons, interactive_environment, better_customization, cube_1, cube_2, cube_3, logo_sketch};
---
+++ client/src/components/cards/ai2-new-feature-card.tsx
@@ -0,0 +1,39 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+interface CardProps {
+  title: string;
+  description: string;
+  image?: string;
+}
+
+export function AI2Card({ title, description, image }: CardProps) {
+  return (
+    <motion.div
+      className="rounded-2xl p-6 shadow-lg h-110 w-80 sm:w-full flex items-center flex-col"
+      style={{ background: 'radial-gradient(circle, rgba(103,128,253,0.3) 0%, rgba(103,128,253,0) 100%)' }}
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true, amount: 0.3 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      whileHover={{
+        scale: 1.05,
+        boxShadow: "0 10px 20px rgba(0,0,0,0.2)",
+      }}
+    >
+      {image && (
+        <div className="relative items-center mb-4 w-60 h-80 mt-10">
+          <Image
+            src={image}
+            alt={title}
+            fill
+            style={{ objectFit: "contain" }}
+            priority={true}
+          />
+        </div>
+      )}
+      <h3 className="whats-new-h1 text-center">{title}</h3>
+      <p className="mt-2 text-gray-700 text-center">{description}</p>
+    </motion.div>
+  );
+}
