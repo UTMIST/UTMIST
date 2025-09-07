229c6a92d37eea6164b5687edb6a011195f380ca
William Nhut Lam <163342709+williamntlam@users.noreply.github.com>
2025-09-07 13:36:44 -0400

    [BUGFIX]: Redirect User to Profile Page after Successful Login and Registration (#142)
    * feat: changed redirection after login to the profile page and changed the auth middleware to redirect away from the auth page to the profile page if the user has logged in
    
    * revert: removeved middleware chnage as this is not related to the issue at hand.
    
    * Redirect to /profile in middleware.ts
    
    ---------
    
    Co-authored-by: Kenny Cui <kenny.cui@mail.utoronto.ca>

--- client/src/app/auth/callback/route.ts
+++ client/src/app/auth/callback/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
   const type = requestUrl.searchParams.get('type')
-  const next = requestUrl.searchParams.get('next') ?? '/dashboard'
+  const next = requestUrl.searchParams.get('next') ?? '/profile'
 
   if (code) {
     const cookieStore = await cookies()
--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -43,7 +43,7 @@ export async function middleware(request: NextRequest) {
 
   // Redirect authenticated users away from auth page
   if (pathname === "/auth" && user) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+    return NextResponse.redirect(new URL("/profile", request.url));
   }
 
   // Redirect unauthenticated users away from protected routes
