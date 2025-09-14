fc4e202ac6c6f6302d337e89449ea707620f42d4
Albert Huynh <huynhalb@gmail.com>
2025-09-14 13:57:16 -0400

    add button (#153)
    * add button
    
    * fix

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -5,9 +5,11 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../assets/logos/utmist-logo-small.svg";
 import { useState, useEffect } from "react";
+import { useUser } from "@/hooks/useUser";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUser();
 
   useEffect(() => {
     if (isOpen) {
@@ -61,6 +63,11 @@ export default function Navbar() {
           <li>
             <Link href="/eigenai" className="nav-item">
               EigenAI
+            </Link>
+          </li>
+          <li>
+            <Link href={user ? "/profile" : "/auth"} className="nav-button">
+              {user ? "Profile" : "Login"}
             </Link>
           </li>
         </ul>
@@ -117,7 +124,16 @@ export default function Navbar() {
                 <Link href="/eigenai" onClick={() => setIsOpen(false)}>
                   EigenAI
                 </Link>
-              </li> 
+              </li>
+              <li>
+                <Link
+                  href={user ? "/profile" : "/auth"}
+                  onClick={() => setIsOpen(false)}
+                  className="nav-button"
+                >
+                  {user ? "Profile" : "Login"}
+                </Link>
+              </li>
             </ul>
           </div>
         )}
