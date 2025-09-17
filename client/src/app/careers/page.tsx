a20de6daae1d3b06203d88861aefbfe496c4b699
Kenny Cui <kenny.cui@mail.utoronto.ca>
2025-09-16 22:45:25 -0400

    Close Associate Positions (#162)

--- client/src/app/careers/page.tsx
+++ client/src/app/careers/page.tsx
@@ -93,6 +93,10 @@ export default function CareersPage() {
         <div className="open-positions-container">
             <h1 className="open-positions-title">Open Positions</h1>
 
+            <h2 className="open-positions-subtitle">
+                No Positions are open at this time, come back later!
+            </h2>
+
             {positions.map((position, index) => (
                 <div key={index}>
                     <div className="position-card">
