ec67af832730cb6ff46ea40de2ab4775ebfec523
William Lam <williamntlam@gmail.com>
2025-09-19 19:58:34 -0400

    fix: removed unused imports from code pasting

--- client/src/app/ml-fundamentals/page.tsx
+++ client/src/app/ml-fundamentals/page.tsx
@@ -27,55 +27,6 @@ type WorkshopContent = {
 
 import { programDirectors, academicsTeam } from "./data";
 
-import * as speakerPhotos from "@/assets/photos/speakers";
-
-const speakers = [
-  {
-    name: "Amrit Krishnan",
-    role: "Technical Team Lead at Vector Institute",
-    profileURL: "",
-    profileImage: speakerPhotos.Amrit,
-  },
-  {
-    name: "Devang Sharma",
-    role: "Engineer at Amazon",
-    profileURL: "",
-    profileImage: speakerPhotos.Devang,
-  },
-  {
-    name: "Tyler Smith",
-    role: "Software Engineer at AI Warehouse",
-    profileURL: "",
-    profileImage: speakerPhotos.Tyler,
-  },
-  {
-    name: "Priyanka Velagala",
-    role: "Speaker",
-    profileURL: "",
-    profileImage: speakerPhotos.Priyanka,
-  },
-  {
-    name: "Marcelo Ponce",
-    role: "Professor at University of Toronto",
-    profileURL: "",
-    profileImage: speakerPhotos.Marcelo,
-  },
-  {
-    name: "Mustafa Khan",
-    role: "Technical Director at aUToronto",
-    profileURL: "",
-    profileImage: speakerPhotos.Mustafa,
-  },
-
-  {
-    name: "Ryan Koh",
-    role: "Postdoctoral Fellow at UofT/KITE-UHN",
-    profileURL: "",
-    profileImage: speakerPhotos.Ryan,
-  },
-];
-
-
 // Week data for the schedule
 const weekData = [
   {
