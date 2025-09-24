91cb23ffcd9f6e1695d2093a1cd5e8cec85f0c96
Guneev Pannu <guneevpannu@outlook.com>
2025-09-23 23:13:24 -0400

    Finished member card

--- client/src/app/departments/page.tsx
+++ client/src/app/departments/page.tsx
@@ -1,4 +1,3 @@
-// import Home from "client/src/app/layout.tsx";
 import {PersonCard} from "@/components/memberList"
 
 export default function Template() {
@@ -7,6 +6,7 @@ export default function Template() {
             <PersonCard
                 name = "Ambrose Ling"
                 text = "Awesome flavour text lorme ipismdifa ajdn askjdnaklsjdnb lhjkab jhkl bndka bdak jn"
+                email = "test@mail.com"
             />
         </>
     )
---
+++ client/src/components/memberList.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import "@/styles/memberList.css"
+import Image from "next/image"
+import Mail from "@/assets/icons/email.svg"
+import {useState} from "react";
+
+interface Props {
+    name: string;
+    text: string;
+    email: string;
+}
+
+
+export function PersonCard({ name, text, email }: Props) {
+
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [isCopied, setIsCopied] = useState(false);
+
+    const handleCopy = async () => {
+        await navigator.clipboard.writeText(email)
+        setIsCopied(true)
+        // console.log("Copied :" + isCopied)
+        setTimeout(() => setIsCopied(false), 2000)
+    }
+
+    return (
+        <div className={"person-card"}>
+            <p className={'person-name'}>{name}</p>
+            <button onClick={() => setIsExpanded(!isExpanded)}>
+                <p className={'person-text'}
+                    style={{
+                        transition: "max-height 0.4s ease",
+                        maxHeight: isExpanded ? "150px" : "40px",
+                }}
+                >{text}</p>
+            </button>
+
+            <button className={'mail-icon'} onClick={() => handleCopy()}>
+                <Image
+                    src={Mail}
+                    alt={'Mail icon'}
+                    width={30}
+                />
+                <div className={'copy-me'}>
+                    {!isCopied ? "Click to copy!" : "Copied"}
+                </div>
+            </button>
+
+
+        </div>
+    )
+}
+
