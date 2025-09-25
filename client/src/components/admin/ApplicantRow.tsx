53c55f29a02bf1502d8c27f40a8b784bb9a01e04
shubhmgrg <shubham.garg@mail.utoronto.ca>
2025-09-25 19:34:57 -0400

    Fixed type issues

--- client/src/components/admin/ApplicantRow.tsx
+++ client/src/components/admin/ApplicantRow.tsx
@@ -17,7 +17,7 @@ export default function ApplicantRow({applicant}: {applicant: Applicant}) {
             <tr className={`transition-all overflow-hidden duration-300 ease-in-out`}>
                 <td className="px-4 py-2"><a className="font-semibold underline text-blue-500" href={`/applicants/${applicant.id}`}>{applicant.name}</a></td>
                 <td className="px-4 py-2">{applicant.role}</td>
-                <td className={`px-4 py-2`}><div className={`max-h-12 overflow-hidden ${expanded ? "max-h-full" : "transition-all duration-300 ease-in-out"}`}>{applicant.summary}</div></td>
+                <td className={`px-4 py-2`}><div className={`max-h-12 overflow-hidden ${expanded ? "max-h-full" : "transition-all duration-300 ease-in-out"}`}>{applicant.notes}</div></td>
                 <td className="px-4 py-2">
                     <span className={`px-2 py-1 rounded text-xs ${
                         applicant.interviewStatus === "Scheduled"
