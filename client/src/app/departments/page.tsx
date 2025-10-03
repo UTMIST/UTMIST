c6e60eb51765069ec3c598fb75cef36ba9f42a81
Guneev Pannu <guneevpannu@outlook.com>
2025-10-03 18:40:56 -0400

    fixes: added arrow to email tooltip, general cleanup

--- client/src/app/departments/page.tsx
+++ client/src/app/departments/page.tsx
@@ -1,30 +1,12 @@
-import {PersonCard, MemberList} from "@/components/memberList"
-
-interface Member {
-    name: string;
-    bio: string;
-    email: string;
-}
-
-const members: Member[] = [
-    {
-        name : "Ambrose Ling",
-        bio : "Awesome flavour text lorme ipismdifa ajdn askjdnaklsjdnb lhjkab jhkl bndka bdak jn",
-        email : "test@mail.com"
-    },
-    {
-        name : "Ambrose Ling",
-        bio : "Awesome flavour text lorme ipismdifa ajdn askjdnaklsjdnb lhjkab jhkl bndka bdak jn",
-        email : "test@mail.com"
-    }
-]
-
+import {PersonCard} from "@/components/memberList"
 
 export default function Test() {
     return (
         <>
-            <MemberList
-                members = {members}
+            <PersonCard
+                name={"Ambrose Ling"}
+                bio={"Look how much it space it can hold, now each member can give a taste of their personality to their card!"}
+                email={"myEmail@mail.com"}
             />
         </>
     )
