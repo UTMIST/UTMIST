468a393286bbcde4fde2076920b1824758f3e031
Guneev Pannu <guneevpannu@outlook.com>
2025-10-23 22:34:20 -0400

    Created InitiativeCard

--- client/src/app/departments/page.tsx
+++ client/src/app/departments/page.tsx
@@ -1,4 +1,5 @@
 import {MemberList} from "@/components/memberList"
+import {InitiativeCard} from "@/components/initiatives"
 import {MemberGroup} from "@/types/departments";
 
 const members: MemberGroup[] = [
@@ -33,11 +34,17 @@ const members: MemberGroup[] = [
 ]
 
 
-
 export default function DepartmentsPage() {
     return (
         <>
             <MemberList members={members} />
+           <hr className={"pt-20 pb-20"}/>
+            <InitiativeCard
+                title={"UTMIST Website"}
+                description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."}
+                projectLink={"#"}
+                image={"file.svg"}
+            />
         </>
     )
 }
